e5d0467d49670bb282e4f0f24e80a988cfe92d0a
satsathish <cloud2torial@gmail.com>
2019-11-16 08:40:51 +0530

    backup

--- config/credentials.js
+++ config/credentials.js
@@ -1,7 +1,7 @@
 var secrets={
     google: {
-        clientID: '578765489852-ihotevf6rqqqv19t5vd3pla5ms2in2ov.apps.googleusercontent.com',
-        clientSecret: 'ilnauIO4phiQ5-y4uBum8Uf7',
+        clientID: '578765489852-7s607cen3efekaor6f8rpbnc7tos1osf.apps.googleusercontent.com',
+        clientSecret: 'XiYRtZjMZ4ldpvfRzKiFqIfE',
         callbackURL: 'http://localhost:1337/user/googlecallback'
       }
     }
